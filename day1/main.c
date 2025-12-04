3fd1c195a0a70e8fdf8a5e1d98f55c639af1599e
yavidor <yonatan@avidor.org>
2025-12-05 01:26:26 +0200

    Trying day 1

--- day1/main.c
+++ day1/main.c
@@ -3,18 +3,38 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "../utils/utils.h"
+
+#define RIGHT 'R'
+#define LEFT 'L'
+
+int readNumber(const char *str, int pos) {
+    const int newLinePos = nextNewline(str, pos) + 1;
+    char *numInput = malloc(sizeof(char) * 1+(newLinePos - pos));
+    int innerPosition = 0;
+    pos++;
+
+    while (pos < newLinePos-1) {
+        *(numInput + innerPosition) = *(str + pos);
+        innerPosition++;
+        pos++;
+    }
+    *(numInput + innerPosition) = '\0';
+    char *endptr = (numInput + innerPosition);
+    long longoosh = strtol(numInput, &endptr, 10);
+    free(numInput);
+    return longoosh;
+}
 
 int main(void) {
     int pos = 0;
     const char *input = readLines(1, false);
-    //Loop until the end of the file
-    while (pos < lenstr(input)) {
-        printf("%c\n", *(input + pos));
-        if (*(input + pos) == 'R')
-            printf("Hello\n");
-        else
-            printf("Goodbye\n");
+    // Loop until the end of the file
+    while (pos < lenstr(input)-1) {
+        const bool positiveDirection = *(input + pos) == RIGHT;
+        printf("%d\n", positiveDirection);
+        printf("%d\n", readNumber(input, pos));
         pos = nextNewline(input, pos) + 1;
     }
 }
