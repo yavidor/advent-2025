7ff2df0bbf193fbf71ce38f7b4534f1abb5f20a8
yavidor <yonatan@avidor.org>
2025-12-05 15:27:38 +0200

    Solved part 2

--- day1/main.c
+++ day1/main.c
@@ -53,10 +53,41 @@ int part1(const char *input) {
     return zeroCounter;
 }
 
+int part2(const char *input) {
+    int pos = 0;
+    const int inputLength = lenstr(input);
+
+    // The value on the lock, starting position is 50
+    int value = 50;
+
+    // Times the lock's value was zero
+    int zeroCounter = 0;
+
+    // Loop until the end of the file
+    while (pos < inputLength - 1) {
+        const bool positiveDirection = *(input + pos) == RIGHT;
+        const int rotation = readNumber(input, pos);
+        int stepsCounter = 0;
+        while (stepsCounter < rotation) {
+            value += positiveDirection ? 1 : -1;
+            value = value >= 0 ? value : value + 100;
+            value %= 100;
+            if (value == 0) {
+                zeroCounter++;
+            }
+            stepsCounter++;
+        }
+        pos = nextNewline(input, pos, inputLength) + 1;
+    }
+    return zeroCounter;
+}
+
 int main(void) {
     const char *input = readLines(1, true);
 
     int part1Output = part1(input);
-
     printf("%d\n", part1Output);
+
+    int part2Output = part2(input);
+    printf("%d\n", part2Output);
 }
